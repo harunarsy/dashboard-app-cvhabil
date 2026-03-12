af581bb9a3ae74444be54dfd7ca058fe42c4351d
Harun <habilsejahterabersama@gmail.com>
2026-03-12 11:58:58 +0700

    chore(prod): backend and infrastructure cloud readiness

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,17 @@
 const { Pool } = require('pg');
 
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL 
+    ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
+    : {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+      }
+);
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
